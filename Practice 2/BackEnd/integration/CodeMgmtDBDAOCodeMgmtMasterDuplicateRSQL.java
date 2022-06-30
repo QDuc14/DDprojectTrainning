48816868ae76a1eac1dfab5ab51b2397da339ef3
Duc <ddquach14@gmail.com>
2022-06-30 09:46:21 +0700

    update practice 2

---
+++ Practice 2/BackEnd/integration/CodeMgmtDBDAOCodeMgmtMasterDuplicateRSQL.java
@@ -0,0 +1,76 @@
+/*=========================================================
+*Copyright(c) 2022 CyberLogitec
+*@FileName : CodeMgmtDBDAOCodeMgmtMasterDuplicateRSQL.java
+*@FileTitle : 
+*Open Issues :
+*Change history :
+*@LastModifyDate : 2022.06.23
+*@LastModifier : 
+*@LastVersion : 1.0
+* 2022.06.23 
+* 1.0 Creation
+=========================================================*/
+package com.clt.apps.opus.dou.doutraining.codemgmt.integration;
+
+import java.util.HashMap;
+import org.apache.log4j.Logger;
+import com.clt.framework.support.db.ISQLTemplate;
+
+/**
+ *
+ * @author Quach Dai Duc
+ * @see DAO 참조
+ * @since J2EE 1.6
+ */
+
+public class CodeMgmtDBDAOCodeMgmtMasterDuplicateRSQL implements ISQLTemplate{
+
+	private StringBuffer query = new StringBuffer();
+	
+	Logger log =Logger.getLogger(this.getClass());
+	
+	/** Parameters definition in params/param elements */
+	private HashMap<String,String[]> params = null;
+	
+	/**
+	  * <pre>
+	  * select master
+	  * </pre>
+	  */
+	public CodeMgmtDBDAOCodeMgmtMasterDuplicateRSQL(){
+		setQuery();
+		params = new HashMap<String,String[]>();
+		String tmp = null;
+		String[] arrTmp = null;
+		tmp = java.sql.Types.VARCHAR + ",N";
+		arrTmp = tmp.split(",");
+		if(arrTmp.length !=2){
+			throw new IllegalArgumentException();
+		}
+		params.put("intg_cd_id",new String[]{arrTmp[0],arrTmp[1]});
+
+		query.append("/*").append("\n"); 
+		query.append("Path : com.clt.apps.opus.dou.doutraining.codemgmt.integration").append("\n"); 
+		query.append("FileName : CodeMgmtDBDAOCodeMgmtMasterDuplicateRSQL").append("\n"); 
+		query.append("*/").append("\n"); 
+	}
+	
+	public String getSQL(){
+		return query.toString();
+	}
+	
+	public HashMap<String,String[]> getParams() {
+		return params;
+	}
+
+	/**
+	 * Query 생성
+	 */
+	public void setQuery(){
+		query.append("SELECT " ).append("\n"); 
+		query.append("	INTG_CD_ID" ).append("\n"); 
+		query.append("FROM COM_INTG_CD" ).append("\n"); 
+		query.append("WHERE	INTG_CD_ID = @[intg_cd_id]" ).append("\n"); 
+
+	}
+}
